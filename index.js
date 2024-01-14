6fc9df5761068d84b0168e885aebede897ed6c19
Bassam Msmar <bassammsmar@gmail.com>
2024-01-14 13:36:44 +0300

    add tow file about js

---
+++ index.js
@@ -0,0 +1,33 @@
+let student = "Bassam"
+let ageAhmed = 12
+let ageAli = 18
+let isMarried = true
+
+let color = null
+
+let phoneNumber 
+
+let ages = ageAhmed  + ageAli
+
+let stringAges = String(ages)
+let numberAges = Number(ages)
+
+
+
+console.log(student)
+console.log(ages)
+console.log(stringAges)
+console.log(numberAges)
+
+
+console.log(typeof  student)
+console.log(typeof  ages)
+console.log(typeof  isMarried)
+console.log(typeof  color)
+console.log(typeof  phoneNumber)
+console.log(typeof  stringAges)
+console.log(typeof  numberAges)
+
+
+
+console.log(`Hi ${student}`)
